2dfc67e20d6d2d4959328d098cacd77cbefd823c
StarfallV <63696126+StarfallV@users.noreply.github.com>
2022-08-16 17:19:08 +0700

    WDOGE

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -10,9 +10,6 @@ export const WETH9: { [chainId: number]: Token } = {
   [5]: new Token(5, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
   [42]: new Token(42, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
 
-  [10]: new Token(10, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
-  [69]: new Token(69, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
-
-  [42161]: new Token(42161, '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', 18, 'WETH', 'Wrapped Ether'),
-  [421611]: new Token(421611, '0xB47e6A5f8b33b3F17603C83a0535A9dcD7E32681', 18, 'WETH', 'Wrapped Ether')
+  [10]: new Token(2000, '0xbA910a7E6D63212Cad7C14A9A946F283e7FE4a35', 18, 'WWDOGE', 'Wrapped WDOGE'),
+  [69]: new Token(568, '0xbA910a7E6D63212Cad7C14A9A946F283e7FE4a35', 18, 'WWDOGE', 'Wrapped WDOGE')
 }
